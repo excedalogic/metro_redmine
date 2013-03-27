a1bdbe3c1262d1f138a35f8e89b74dbc39936732
Gopinath <gopinath.p@excedalogic.com>
2013-03-27 10:53:00 +0530

    First Commit
    Metro Redmine Theme.

---
+++ javascripts/theme.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    var $newElement = $('<a href="http://www.eis.ms">Excedalogic</a>');
+    var div = document.getElementById("footer");
+    $(div).append(document.createTextNode("Metro Redmine Theme by ")).append($newElement);
+    
+});
